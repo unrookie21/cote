bfaf3fd155efa86adcf10a02726bea173ccbc684
unrookie21 <piresj0318@gmail.com>
2025-11-02 22:34:03 +0900

    잔돈 교환 - BFS

---
+++ etoc/src/Cote1102.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Cote1102 {
+
+    class Solution {
+        public int solution(int[] coins, int amount) {
+            Deque<int[]> queue = new ArrayDeque<>();
+            Set<Integer> visited = new HashSet<>();
+
+            queue.offer(new int[]{amount, 0});
+            visited.add(amount);
+
+            while(!queue.isEmpty()){
+                int[] current = queue.poll();
+                int curAmount = current[0];
+                int count = current[1];
+
+                if (curAmount == 0) return count;
+
+                for (int coin : coins){
+                    int nextAmount = curAmount - coin;
+                    if (nextAmount >= 0 && !visited.contains(nextAmount)){
+                        queue.offer(new int[]{nextAmount, count + 1});
+                        visited.add(nextAmount);
+                    }
+
+                }
+            }
+
+            return -1;
+        }
+    }
+}
