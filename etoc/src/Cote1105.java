a28c9e04cd534cfe571584f232cc33aabaac2df3
unrookie21 <piresj0318@gmail.com>
2025-11-06 23:53:16 +0900

    BOJ 2178 : 미로탐색(목적지까지 최단경로구하기) solved by BFS

---
+++ etoc/src/Cote1105.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Cote1105 {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>(List.of(3,2,1,1));
+        Set<Integer> set = new HashSet<>(list);
+        for (Integer i : set) {
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+}
