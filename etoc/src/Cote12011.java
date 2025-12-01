28d2f1fc51e121cb9e339ecdd951a7202eb6038b
unrookie21 <piresj0318@gmail.com>
2025-12-01 16:17:41 +0900

    hashset : 전화번호 목록

---
+++ etoc/src/Cote12011.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Cote12011 {
+
+    class Solution {
+        public boolean solution(String[] phone_book) {
+            boolean answer = true;
+
+            Set<String> set = new HashSet<>();
+            for (String number : phone_book){
+                set.add(number);
+            }
+
+            for (String number : phone_book){
+                for (int i = 1; i < number.length(); i++){
+                    if (set.contains(number.substring(0,i))){
+                        return false;
+                    }
+                }
+
+            }
+            return answer;
+        }
+    }
+
+}
