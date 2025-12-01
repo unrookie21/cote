81c2c0c94c0851be512657bb44e70051f970f3ed
unrookie21 <piresj0318@gmail.com>
2025-12-01 16:53:00 +0900

    hasmap : 의상의 조합 구하기

---
+++ etoc/src/Cote12012.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Cote12012 {
+
+    class Solution {
+        public int solution(String[][] clothes) {
+
+            int answer = 1;
+
+            Map<String, Integer> map = new HashMap<>();
+
+            for (String[] cloth : clothes){
+                map.put(cloth[1], map.getOrDefault(cloth[1] , 0) + 1);
+            }
+
+            for (String key : map.keySet()){
+                answer *= map.get(key) + 1;
+            }
+
+            return answer - 1;
+        }
+    }
+}
