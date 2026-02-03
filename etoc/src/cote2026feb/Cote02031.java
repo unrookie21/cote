b67194c27b163c083afd0cb04795c55876dc6635
unrookie21 <piresj0318@gmail.com>
2026-02-03 15:16:13 +0900

    leet code : two sum - hash table 복습

---
+++ etoc/src/cote2026feb/Cote02031.java
@@ -0,0 +1,28 @@
+package cote2026feb;
+
+import java.util.*;
+public class Cote02031 {
+
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+
+
+            Map<Integer , Integer> map = new HashMap<>();
+
+            for(int i = 0 ; i<nums.length ; i++){
+
+                int require =target -  nums[i] ;
+
+                if(map.containsKey(require)){
+                    return new int[] {map.get(require) , i};
+
+                }
+
+                map.put(nums[i] , i );
+
+
+            }
+            return null;
+        }
+    }
+}
