4de5f2bc2eec50a845de7386cff7bec2606ebb54
unrookie21 <piresj0318@gmail.com>
2026-02-13 16:12:16 +0900

    leet code : Longest Increasing Subsequence - DP (dp 가 너무 싫다.)

---
+++ etoc/src/cote2026feb/Cote02131.java
@@ -0,0 +1,30 @@
+package cote2026feb;
+
+import java.util.*;
+public class Cote02131 {
+
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+
+            int n = nums.length;
+            int[] dp = new int[n];
+            Arrays.fill(dp, 1);
+
+
+            int result = 1;
+            for (int i = 1; i < n; i++){
+                for (int j = 0; j < i; j++){
+                    if (nums[j] < nums[i]){
+                        dp[i] =  Math.max(dp[i] , dp[j] + 1);
+                    }
+                }
+
+                result = Math.max(result, dp[i]);
+
+            }
+
+            return result;
+
+        }
+    }
+}
