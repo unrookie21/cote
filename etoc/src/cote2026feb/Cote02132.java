c1da1513089e1700942dcf6be234edc0aaf3fd30
unrookie21 <piresj0318@gmail.com>
2026-02-13 16:12:57 +0900

    leet code : Longest Common Subsequence - DP (역대급으로 어렵네)

---
+++ etoc/src/cote2026feb/Cote02132.java
@@ -0,0 +1,31 @@
+package cote2026feb;
+
+public class Cote02132 {
+
+    class Solution {
+        public int longestCommonSubsequence(String text1, String text2) {
+
+            int m = text1.length();
+            int n = text2.length();
+
+            int[][] dp = new int[m+1][n+1];
+
+            for (int i = 1; i <= m; i++){
+                char c1 = text1.charAt(i - 1);
+                for (int j = 1; j <= n; j++){
+                    char c2 = text2.charAt(j -1);
+
+                    if (c1 == c2){
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    } else{
+                        dp[i][j] = Math.max(dp[i][j-1] , dp[i-1][j]);
+                    }
+
+                }
+            }
+
+            return dp[m][n];
+
+        }
+    }
+}
